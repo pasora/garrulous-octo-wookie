c5e60bb544080cfff9e721a0d0d9b2e394b8da29
Masahiko Hara <pasora@sfc.wide.ad.jp>
2015-11-12 22:10:15 +0900

    add SuicaLog.java

--- app/src/main/java/jp/gr/java_conf/pasora/orf2015/SuicaLog.java
+++ app/src/main/java/jp/gr/java_conf/pasora/orf2015/SuicaLog.java
@@ -1,13 +1,17 @@
 package jp.gr.java_conf.pasora.orf2015;
 
-import java.util.Date;
+import android.util.Log;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by masahikohara on 2015/11/05.
  */
+
 public class SuicaLog {
     private byte[] suicaLogBin;
-    private Date date;
+    private Calendar date;
     private String enter;
     private String exit;
     private String latitude;
@@ -15,13 +19,24 @@ public class SuicaLog {
 
     public SuicaLog(byte[] suicaLogStr) {
         this.suicaLogBin = suicaLogStr;
+        setDate();
 
     }
 
-    void setDate() {
-        int year = this.suicaLogBin & 0x1F;
-        int month;
-        int day;
+    boolean isToday() {
+        Calendar now = Calendar.getInstance();
+        String nowStr = new java.text.SimpleDateFormat("yyyy-MM-dd", Locale.JAPAN).format(now.getTime());
+        String dateStr = new java.text.SimpleDateFormat("yyy-MM-dd", Locale.JAPAN).format(date.getTime());
+        return nowStr.equals(dateStr);
+    }
 
+    void setDate() {
+        int year = (this.suicaLogBin[4] >> 1) + 2000;
+        int month = ((this.suicaLogBin[4] & (byte)0x01) << 3) + ((this.suicaLogBin[5] & (byte)0xE0) >> 5);
+        int day = this.suicaLogBin[5] & (byte) 0x1F;
+        this.date.set(year, month, day);
+        Log.d("date_year", String.valueOf(year));
+        Log.d("date_month", String.valueOf(month));
+        Log.d("date_day", String.valueOf(day));
     }
 }
