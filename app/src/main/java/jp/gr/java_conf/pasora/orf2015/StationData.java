546557fb6cff3fa3062652260a970b8a827ae281
Masahiko Hara <pasora@sfc.wide.ad.jp>
2015-11-15 01:39:30 +0900

    create station database class

---
+++ app/src/main/java/jp/gr/java_conf/pasora/orf2015/StationData.java
@@ -0,0 +1,21 @@
+package jp.gr.java_conf.pasora.orf2015;
+
+/**
+ * Created by masahikohara on 2015/11/15.
+ */
+public class StationData {
+    private String[] regionCode = new String[5952];
+    private String[] sectionCode = new String[5952];
+    private String[] stationCode = new String[5952];
+    private String[] companyName = new String[5952];
+    private String[] lineName = new String[5952];
+    private String[] stationName = new String[5952];
+    private String[] latitude = new String[5952];
+    private String[] longitude = new String[5952];
+    private String[] address = new String[5952];
+    private String[] other = new String[5952];
+
+    public StationData() {
+
+    }
+}
